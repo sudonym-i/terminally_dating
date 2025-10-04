23610e88f50449755b7641747fc246efb6a1e55a
sudonym-i <isickln08@gmail.com>
2025-10-04 14:51:46 -0600

    added some styles. bio, and github link

--- ascii_stuff/UI.py
+++ ascii_stuff/UI.py
@@ -1,5 +1,31 @@
+import profile
 import pyfiglet
 import os
+
+# Soft Retro Gruvbox color scheme
+class Colors:
+    RESET = '\033[0m'
+    # Foreground colors
+    BLACK = '\033[38;2;40;40;40m'       # black/dark
+    BG = '\033[38;2;40;40;40m'          # gruvbox dark bg
+    FG = '\033[38;2;213;196;161m'       # gruvbox warm fg
+    RED = '\033[38;2;204;143;129m'      # subtle red
+    GREEN = '\033[38;2;164;167;58m'     # subtle green
+    YELLOW = '\033[38;2;215;169;87m'    # subtle yellow
+    BLUE = '\033[38;2;121;145;142m'     # subtle blue
+    PURPLE = '\033[38;2;181;124;145m'   # subtle purple
+    AQUA = '\033[38;2;132;172;114m'     # subtle aqua
+    ORANGE = '\033[38;2;214;138;75m'    # subtle orange
+    # Background colors
+    BG_RED = '\033[48;2;204;143;129m'      # subtle red bg
+    BG_GREEN = '\033[48;2;164;167;58m'     # subtle green bg
+    BG_YELLOW = '\033[48;2;215;169;87m'    # subtle yellow bg
+    BG_BLUE = '\033[48;2;121;145;142m'     # subtle blue bg
+    BG_PURPLE = '\033[48;2;181;124;145m'   # subtle purple bg
+    BG_AQUA = '\033[48;2;132;172;114m'     # subtle aqua bg
+    BG_ORANGE = '\033[48;2;214;138;75m'    # subtle orange bg
+    BG_DARK = '\033[48;2;40;40;40m'        # dark bg
+    BG_WHITE = '\033[48;2;255;255;255m'    # white bg
 
 
 fonts = { "electronic", "dos_rebel", "def_leppard", "sweet", "5lineoblique", "bigmono9", "lean", 
@@ -11,9 +37,6 @@ fonts = { "electronic", "dos_rebel", "def_leppard", "sweet", "5lineoblique", "bi
 class UI:
     def __init__(self):
 
-        os.system('clear')
-        print("\n\n")
-
         self.fonts = { "electronic", "dos_rebel", "def_leppard", "sweet", "5lineoblique", "bigmono9", "lean", 
              "georgi16", "3d-ascii", "georgia11", "banner3", "new_asci", "the_edge", "nscript",
              "cybermedium", "big_money-nw", "starwars", "pagga", "delta_corps_priest_1", "rozzo", "sub-zero",
@@ -22,39 +45,74 @@ class UI:
 
     def print_profile( self, profile):
 
+        os.system('clear')
+        term_width = os.get_terminal_size().columns
+        print("\n\n")
+        print("\n\n" + Colors.AQUA + "\\"*term_width + Colors.RESET + "\n\n")
+
 
         # Print profile NAME and PICTURE
+
+
+        # Print PROFILE 
         f =  pyfiglet.figlet_format( profile.user_name , font=profile.name_font)
+
         figlet_lines = f.split('\n')
 
-        term_width = os.get_terminal_size().columns
         left_margin = 5
         right_margin = 5
 
-        # Print figlet and square side by side
-
+        # Print figlet and square side by side (have to to make them side by side)
         for i in range(max(len(figlet_lines), 18)):
             # Print left margin
             print(' ' * left_margin, end='')
 
-            # Print figlet line (or empty if exhausted)
+            # Print figlet line (or empty if exhausted) in orange
             if i < len(figlet_lines):
                 figlet_line = figlet_lines[i]
-                print(figlet_line, end='')
+                print(Colors.YELLOW + figlet_line + Colors.RESET, end='')
                 spacing = term_width - left_margin - len(figlet_line) - 46 - right_margin
             else:
                 spacing = term_width - left_margin - 46 - right_margin
 
-            # Print square line on the same row
+            # Print square line on the same row in purple
             if i < 18:
-                print(' ' * spacing + '█' * 46)
+                print(' ' * spacing + Colors.RESET + '█' * 46 + Colors.RESET)
             else:
                 print()
 
 
         # Print description/bio and other info
 
-        print("\n" + "/"*term_width + "\n")
+        print("\n\n" + Colors.AQUA + "/"*term_width + Colors.RESET + "\n\n")
+
+
+        print(Colors.BG_ORANGE + Colors.BLACK + "About me:" + Colors.RESET + "\n")
+
+        # Wrap bio text to half screen width
+        max_width = term_width // 2
+        words = profile.bio.split()
+        lines = []
+        current_line = ""
+
+    # for loop to format our text to stay only on the left side of the screen
+        for word in words:
+            if len(current_line) + len(word) + 1 <= max_width:
+                current_line += f"{word} "
+            else:
+                lines.append(current_line.rstrip())
+                current_line = f"{word} "
+        if current_line:
+            lines.append(current_line.rstrip())
+
+
+        
+        print(f"{Colors.FG}{'\n'.join(lines)}{Colors.RESET}", end='')
+
+        # Calculate spacing to align github to the right
+        github_text = profile.github
+        spacing = term_width - len(lines[-1]) - len("GITHUB: " + github_text)
+        print(f"{' ' * spacing}{Colors.ORANGE}GITHUB: {github_text}{Colors.RESET}")
 
 
         return
--- ascii_stuff/test.py
+++ ascii_stuff/test.py
@@ -1,17 +1,19 @@
 
 from UI import UI
 
+## mock profile class for testing
 
 
 class Profile:
     def __init__(self, user_name, name_font):
         self.user_name = user_name
         self.name_font = name_font
-
+        self.bio = "Hello! I'm " + user_name + ". I love coding, hiking, and exploring new technologies.  I am a prominent figure on the reinmann sum of nerds podcast, and enjoy misleading students in my plp sessions"
+        self.github = "https://github.com/Exahilosys/survey"
         
 if __name__ == "__main__":
 
-    person = Profile("Bryan Holl", "starwars")
+    person = Profile("Bryan Holl", "delta_corps_priest_1")
     ui = UI()
 
     ui.print_profile(person)
