91e3f70f04c616ceff6495f36bd54a690b73875a
sudonym-i <isickln08@gmail.com>
2025-10-05 05:03:38 -0600

    { =

--- ascii_stuff/main.py
+++ ascii_stuff/main.py
@@ -1,5 +1,7 @@
 import sys
-from ..bryan_data_for_ascii.help_me import retrieve_usr
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from bryan_data_for_ascii.help_me import retrieve_usr
 
 """
 Test Module - Mock Implementation for Frontend Testing
